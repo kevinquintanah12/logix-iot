071d7af94cd15e02f7d153d60fc77ef270f1b2e4
kevinquintanah12 <kevinherreraq12@gmail.com>
2025-03-12 18:38:04 -0600

    broker mqtt y server configurado

---
+++ mqtt-simulator/broker/broker.py
@@ -0,0 +1,15 @@
+import subprocess
+import os
+
+MOSQUITTO_CONFIG_PATH = "config/mosquitto.conf"
+
+def start_broker():
+    """Inicia el broker Mosquitto con la configuración personalizada."""
+    try:
+        print("Iniciando broker MQTT...")
+        subprocess.run(["mosquitto", "-c", MOSQUITTO_CONFIG_PATH], check=True)
+    except Exception as e:
+        print(f"Error iniciando el broker: {e}")
+
+if __name__ == "__main__":
+    start_broker()
